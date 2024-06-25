45baf725a8f97f8683f98401e742be40cb35028d
Dhivyan-kumar <156393614+Dhivyan-kumar@users.noreply.github.com>
2024-06-25 12:57:48 +0530

    Update _nav.js-delaware only

--- src/_nav.js
+++ src/_nav.js
@@ -20,125 +20,7 @@ const _nav = [
     component: CNavTitle,
     name: 'Apps',
   },
-  {
-    component: CNavGroup,
-    name: 'Orders@',
-    to: '/dashboard',
-    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
-    badge: {
-      text : "2",
-      color: "warning"
-    },
-    items: [
-      {
-        component: CNavItem,
-        name: 'New',
-        to: '/base/carousels',
-      },
-      {
-        component: CNavItem,
-        name: 'Search',
-        to: '/base/collapses',
-      },
-      {
-        component: CNavItem,
-        name: 'Open',
-        to: '/base/open',
-      },
-      {
-        component: CNavItem,
-        name: 'Group',
-        to: '/base/breadcrumbs',
-      },
-      {
-        component: CNavItem,
-        name: 'Assigned',
-        to: '/base/cards',
-      }
-    ],
-  },
-  {
-    component: CNavGroup,
-    name: 'IT Help',
-    to: '/theme/colors',
-    icon: <CIcon icon={cilDrop} customClassName="nav-icon" />,
-    badge: {
-      text : "7",
-      color: "warning"
-    },
-    items: [
-      {
-        component: CNavItem,
-        name: 'Open',
-        to: '/base/carousels',
-      },
-      {
-        component: CNavItem,
-        name: 'Search',
-        to: '/base/collapses',
-      },
-      {
-        component: CNavItem,
-        name: 'Queue',
-        to: '/base/jumbotrons',
-      }
-    ],
-  },
-  {
-    component: CNavGroup,
-    name: 'RASAccount',
-    to: '/theme/typography',
-    icon: <CIcon icon={cilPencil} customClassName="nav-icon" />,
-    badge: {
-      text : "4",
-      color: "warning"
-    },
-    items: [
-      {
-        component: CNavItem,
-        name: 'Open',
-        to: '/base/navbars',
-      },
-      {
-        component: CNavItem,
-        name: 'Search',
-        to: '/base/navs',
-      },
-      {
-        component: CNavItem,
-        name: 'Queue',
-        to: '/base/paginations',
-      }
-    ],
-  },
-  {
-    component: CNavGroup,
-    name: 'SWAT',
-    to: '/base',
-    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
-    badge: {
-      text : "6",
-      color: "warning"
-    },
-    items: [
-      {
-        component: CNavItem,
-        name: 'Open',
-        to: '/base/accordion',
-      },
-      {
-        component: CNavItem,
-        name: 'Search',
-        to: '/base/breadcrumbs',
-      },
-      {
-        component: CNavItem,
-        name: 'Queue',
-        to: '/base/cards',
-      }
-    ],
-  },
-  {
+    {
     component: CNavGroup,
     name: 'DelawareCrop',
     to: '/buttons',
@@ -160,7 +42,7 @@ const _nav = [
         },
         {
           component: CNavItem,
-          name: 'Queue',
+          name: 'Assigned',
           to: '/base/cards',
         }
       ],
